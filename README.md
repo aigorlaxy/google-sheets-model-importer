34dbee9f7d98883f94c5a9500e5aa7b1231bbc4d
igor1523 <igor1523@gmail.com>
2024-07-08 07:03:13 -0300

    init commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Google Sheets Model Importer
+
+[![Latest Version on Packagist](https://img.shields.io/packagist/v/aigorlaxy/google-sheets-model-importer.svg?style=flat-square)](https://packagist.org/packages/aigorlaxy/google-sheets-model-importer)
+[![Total Downloads](https://img.shields.io/packagist/dt/aigorlaxy/google-sheets-model-importer.svg?style=flat-square)](https://packagist.org/packages/aigorlaxy/google-sheets-model-importer)
+
+A simple trait to insert or update data to models from Google Sheets with just the link of the published spreadsheet. No need of Google API.
+
+## Installation
+You can install the package via composer:
+
+```bash
+composer require aigorlaxy/google-sheets-model-importer
+```
+
+## Using
+
+1. Create a Google Spreadsheet.
+
+2. Publish it to internet.
+
+3. Include Aigorlaxy\HasGoogleSheets trait in your model class.
+
+```php
+use Aigorlaxy\GoogleSheetsModelImporter\GoogleSheetsModelImporterTrait;
+
+class YourModel extends Model
+{
+    use GoogleSheetsModelImporterTrait;
+
+    // Your model code here
+}
+```
+
+4. Set your antire spreadsheet id using your published Google Sheets link
+The link will look something like that: https://docs.google.com/spreadsheets/d/e/<$googleSpreadSheetId>/something-else
+
+```php
+protected string $googleSpreadSheetId = 'your_google_spreadsheet_id';
+```
+
+5. Set your inside tab gid. If you have just one tab for that model, use the gid as a string. If you have more than one tab for the same model, use array. All models must match the same column schema to work.
+The link will look something like that: https://docs.google.com/spreadsheets/d/<$googleSpreadSheetId>/edit?gid=<$googleSheetId>
+
+```php
+protected string|array $googleSheetId = 'your_google_sheet_id'; // or ['sheet1_gid', 'sheet2_gid']
+
+```
+
+6. Optionaly you can set a different primary key to check for updates. if your primary key is not default id or you want to track updates based on another column. If not set, it will assume that id column is your primary key.
+
+```php
+protected string $updateColumnIndex = 'your_column_index';
+
+```
+
+7. You can also optionaly set columns to be skipped. The app will search for columns that contains any of that strings and will skip the import for that ones. You can set a single string or an array.
+
+```php
+protected string|array $columnsToSkip = 'column_1'; // or ['column_1', 'column_2']
+
+```
+
+8. Create a new object of the model and run any of the available methods.
+
+```php
+$model = new YourModel();
+$model->updateOrCreateFromGoogleSheets(); // Updating and inserting new data.
+$model->getFreshTableFromGoogleSheets(true); // Truncating the current table and inserting the new data.
+```
+
+9. Any issue or suggestions, please send me an e-mail: igor1523@gmail.com
---
+++ src/HasGoogleSheets.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+
+
+trait HasGoogleSheets
+{
+    public function updateOrCreateFromGoogleSheets()
+    {
+        $googleSpreadSheetCsvLinks = $this->getGoogleSpreadSheetCsvLinks();
+        foreach ($googleSpreadSheetCsvLinks as $googleSpreadSheetCsvLink) {
+            $googleSheetsRequestBody = $this->httpGetRequest($googleSpreadSheetCsvLink);
+            $googleSheetsCsvPath = $this->saveGoogleSheetsCsv($googleSheetsRequestBody);
+            $csvData = $this->parseCsv($googleSheetsCsvPath);
+            $this->createModel($csvData);
+        }
+    }
+
+    public function getFreshTableFromGoogleSheets()
+    {
+        self::truncate();
+        $this->updateOrCreateFromGoogleSheets();
+    }
+    private function getGoogleSpreadSheetCsvLinks()
+    {
+        $spreadSheetCsvLinks = [];
+        $spreadSheetId = $this->getSpreadSheetId();
+        $sheetIds = $this->getSheetId();
+
+        foreach ($sheetIds as $sheetId) {
+            $spreadSheetCsvLinks[] = 'https://docs.google.com/spreadsheets/d/' . $spreadSheetId . '/export?format=csv&gid=' . $sheetId;
+        }
+        return $spreadSheetCsvLinks;
+    }
+
+    private function httpGetRequest($googleSpreadSheetCsvLink)
+    {
+        $response = Http::get($googleSpreadSheetCsvLink);
+        return $response->body();
+    }
+
+    private function saveGoogleSheetsCsv($googleSheetsRequestBody)
+    {
+        $filename = Str::slug(class_basename(static::class)) . '-' . now()->format('Y-m-d-H-i-s') . '.csv';
+
+        $tempFilePath = 'temp/' . $filename;
+        \Storage::put($tempFilePath, $googleSheetsRequestBody);
+
+        return storage_path('app/' . $tempFilePath);
+    }
+
+    private function parseCsv($googleSheetsCsvPath)
+    {
+        $csvData = [];
+        if (($handle = fopen($googleSheetsCsvPath, 'r')) !== false) {
+            $headers = fgetcsv($handle, 1000, ',');
+            while (($data = fgetcsv($handle, 1000, ',')) !== false) {
+                $csvData[] = array_combine($headers, $data);
+            }
+            fclose($handle);
+        }
+        \Storage::delete('temp/' . basename($googleSheetsCsvPath));
+        return $csvData;
+    }
+
+    private function createModel($csvData, $update = true)
+    {
+        $headers = array_keys($csvData[0]);
+
+        if ($this->getColumnsToSkip()) {
+
+            $filteredHeaders = array_filter($headers, function ($header) {
+                foreach ($this->columnsToSkip as $columnToSkip) {
+                    if (strpos($header, $columnToSkip) !== false) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+        }
+
+        $createdRows = [];
+        $updateColumnIndex = $this->updateColumnIndex ?? 'id';
+
+        foreach ($csvData as $row) {
+            $filteredRow = array_filter($row, function ($key) use ($filteredHeaders) {
+                return in_array($key, $filteredHeaders);
+            }, ARRAY_FILTER_USE_KEY);
+
+            $createdRows[] = self::updateOrCreate([$updateColumnIndex => $filteredRow[$updateColumnIndex]], $filteredRow);
+        }
+        return $createdRows;
+    }
+
+    private function getSpreadSheetId()
+    {
+        return $this->googleSpreadSheetId;
+    }
+    private function getColumnsToSkip()
+    {
+        return $this->columnsToSkip ? (is_array($this->columnsToSkip) ? $this->columnsToSkip : [$this->columnsToSkip]) : false;
+    }
+
+    private function getSheetId()
+    {
+        return is_array($this->googleSheetId) ? $this->googleSheetId : [$this->googleSheetId];
+    }
+
+    protected function initializeHasGoogleSheets()
+    {
+        $this->checkGoogleSheetsProperties();
+    }
+
+    private function checkGoogleSheetsProperties()
+    {
+        $requiredProperties = ['googleSpreadSheetId', 'googleSheetId'];
+        foreach ($requiredProperties as $property) {
+            if (!property_exists($this, $property)) {
+                throw new \Exception("Class " . get_class($this) . " must define the \${$property} property.");
+            }
+        }
+    }
+}
